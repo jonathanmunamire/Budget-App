a15dce0be0269a7f60d3d6e4d44be1a1b19ffc01
jonathanmunamire <jonathanmunamire@gmail.com>
2023-03-28 15:26:13 +0200

    Set Up controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'splash/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "splash#index"
+  resources :groups, path: 'categories', only: [:index, :show, :new, :create] do
+    resources :payments, only: [:index, :new, :create]
+  end
 end
