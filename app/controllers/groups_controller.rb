0f9d3cf562021070063ce895fd0b8acab7eb6193
jonathanmunamire <jonathanmunamire@gmail.com>
2023-03-28 15:38:17 +0200

    Add controllers actions

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,13 @@
 class GroupsController < ApplicationController
+  def index
+    @categories = Group.all
+  end
+
+  def show
+    @category = Group.find_by(id: params[:id])
+  end
+
+  def new
+    @group = Group.new
+  end
 end
