a1be16749c4ade04fb8abb3ae6dd75f4a686b951
jonathanmunamire <jonathanmunamire@gmail.com>
2023-03-28 12:46:20 +0200

    Fix Linters

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,6 +1,6 @@
 class Payment < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    has_and_belongs_to_many :groups
+  belongs_to :author, class_name: 'User'
+  has_and_belongs_to_many :groups
 
-    validates :name, :amount, :author_id, presence: true
+  validates :name, :amount, :author_id, presence: true
 end
